298a9de3d204abe83f9cb683207fff8ac01127cf
YoussefHussien99 <51840033+youssefa111@users.noreply.github.com>
2024-12-14 15:39:24 +0200

    customize paths of microservices by gatewayServer

--- gatewayserver/src/main/java/com/eazybytes/gatewayserver/GatewayserverApplication.java
+++ gatewayserver/src/main/java/com/eazybytes/gatewayserver/GatewayserverApplication.java
@@ -10,4 +10,21 @@ public class GatewayserverApplication {
 		SpringApplication.run(GatewayserverApplication.class, args);
 	}
 
+	@Bean
+	public RouteLocator eazyBankRouteConfig(RouteLocatorBuilder routeLocatorBuilder){
+		return routeLocatorBuilder.routes()
+				.route(p -> p
+						.path("/eazybank/accounts/**")
+						.filters(f -> f.rewritePath("eazybank/accounts/(?<segment>.*)","/${segment}"))
+						.uri("lb://ACCOUNT"))
+				.route(p -> p
+						.path("/eazybank/loans/**")
+						.filters(f -> f.rewritePath("eazybank/loans/(?<segment>.*)","/${segment}"))
+						.uri("lb://LOANS"))
+				.route(p -> p
+						.path("/eazybank/cards/**")
+						.filters(f -> f.rewritePath("eazybank/cards/(?<segment>.*)","/${segment}"))
+						.uri("lb://CARDS")).build();
+	}
+
 }
