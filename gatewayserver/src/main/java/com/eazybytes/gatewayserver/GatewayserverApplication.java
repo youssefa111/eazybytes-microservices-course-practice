336399f8b5a3e6b93c42c25866e64d1db5542b0f
YoussefHussien99 <51840033+youssefa111@users.noreply.github.com>
2024-12-20 23:46:30 +0200

    -Add APIS connection and response Timeout by the gateway server -Add Retry Pattern by gateway server and by the microservice itself -Add config of timout of circuitBreaker in gateway server

--- gatewayserver/src/main/java/com/eazybytes/gatewayserver/GatewayserverApplication.java
+++ gatewayserver/src/main/java/com/eazybytes/gatewayserver/GatewayserverApplication.java
@@ -1,11 +1,18 @@
 package com.eazybytes.gatewayserver;
 
+import io.github.resilience4j.circuitbreaker.CircuitBreakerConfig;
+import io.github.resilience4j.timelimiter.TimeLimiterConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.circuitbreaker.resilience4j.ReactiveResilience4JCircuitBreakerFactory;
+import org.springframework.cloud.circuitbreaker.resilience4j.Resilience4JConfigBuilder;
+import org.springframework.cloud.client.circuitbreaker.Customizer;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
 
+import java.time.Duration;
 import java.time.LocalDateTime;
 
 @SpringBootApplication
@@ -29,7 +36,14 @@ public class GatewayserverApplication {
 				.route(p -> p
 						.path("/eazybank/loans/**")
 						.filters(f -> f.rewritePath("eazybank/loans/(?<segment>.*)","/${segment}")
-								.addResponseHeader("X-Response-Time", LocalDateTime.now().toString()))
+								.addResponseHeader("X-Response-Time", LocalDateTime.now().toString())
+								.retry(retryConfig ->
+										retryConfig
+										.setRetries(3)
+										.setMethods(HttpMethod.GET)
+										.setBackoff(Duration.ofMillis(100),Duration.ofMillis(1000),2,true)
+								)
+						)
 						.uri("lb://LOANS"))
 
 				.route(p -> p
@@ -37,6 +51,15 @@ public class GatewayserverApplication {
 						.filters(f -> f.rewritePath("eazybank/cards/(?<segment>.*)","/${segment}")
 								.addResponseHeader("X-Response-Time", LocalDateTime.now().toString()))
 						.uri("lb://CARDS")).build();
+	}
+
+
+	// This Config of timeout of Resilience of api call before the counter of circuit breaker increase to  open state
+	@Bean
+	public Customizer<ReactiveResilience4JCircuitBreakerFactory> defaultCustomizer() {
+		return factory->factory.configureDefault(id -> new Resilience4JConfigBuilder(id)
+						.circuitBreakerConfig(CircuitBreakerConfig.ofDefaults())
+				.timeLimiterConfig(TimeLimiterConfig.custom().timeoutDuration(Duration.ofSeconds(4)).build()).build());
 	}
 
 }
